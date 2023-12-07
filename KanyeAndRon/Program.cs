20ae0a94356737863fc28b5af43c2e14d6d99def
ZanyaMyers <myerszanya@gmail.com>
2023-12-06 17:38:22 -0800

    inital commit

---
+++ KanyeAndRon/Program.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace KanyeAndRon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enjoy the following spectacle");
+            Console.WriteLine(); 
+            for(int i = 0; i < 5; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    QuoteGenerator.KanyeQuote();
+                }
+                else
+                {
+                    QuoteGenerator.RonQuote();
+                }
+            }
+        }
+    }
+}
